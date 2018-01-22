5dd4127c894a03a625ed37b71f54a44553ae407e
Danny K Pan <kaiwen006@gmail.com>
2018-01-22 15:49:11 +0000

    added buffered string data output

---
+++ step2/func-two.js
@@ -0,0 +1,30 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  var long = "";
+
+  https.get(requestOptions, function(response) {
+
+    response.setEncoding('utf-8');
+
+    response.on('data', function(data) {
+      long += data;
+      // console.log(data);
+      // console.log('---------------------------chunk------------------------------');
+    });
+
+    response.on('end', function() {
+      console.log('End of stream.');
+      console.log(long);
+    });
+  });
+  // console.log(long);
+}
+
+getAndPrintHTMLChunks();
